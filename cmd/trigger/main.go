e2b151eff262f9e29a20b974c6b35aedeec07742
Marcus Carlsson <Marcus.caarlsson92@gmail.com>
2018-06-15 02:20:28 +0100

    asd

--- cmd/trigger/main.go
+++ cmd/trigger/main.go
@@ -12,6 +12,24 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+// Request from IFTTT
+type Request struct {
+	triggers struct {
+		from string `json:"hours_start"`
+		to   string `json:"hours_stop"`
+	} `json:"triggerFields"`
+}
+
+// Response to IFTTT
+type Response struct {
+	data []ResponseDetail `json:"data"`
+}
+
+type ResponseDetail struct {
+	isOverLimit     bool    `json:"isOverLimit"`
+	greenPercentage float32 `json:"greenPercentage"`
+}
+
 type Result struct {
 	Data Data `json:"data"`
 }
@@ -27,15 +45,9 @@ type Generationmix struct {
 	Percentage float32 `json:"perc"`
 }
 
-type Request struct {
-	triggers struct {
-		from string `json:"hours_start"`
-		to   string `json:"hours_stop"`
-	} `json:"triggerFields"`
-}
-
 type Generationmixes []Generationmix
 
+// AggregateGreenEnergy calculates green energy percentage
 func (g Generationmixes) AggregateGreenEnergy() (res float32) {
 	for _, element := range g {
 		switch element.Fuel {
@@ -46,6 +58,7 @@ func (g Generationmixes) AggregateGreenEnergy() (res float32) {
 	return res
 }
 
+// WithHourMinute sets the hour and minute on a given Time
 func WithHourMinute(now time.Time, hmString string) (time.Time, error) {
 	hourStr := hmString[:2]
 	minuteStr := hmString[3:]
@@ -60,6 +73,18 @@ func WithHourMinute(now time.Time, hmString string) (time.Time, error) {
 	}
 
 	return time.Date(now.Year(), now.Month(), now.Day(), hour, minute, now.Second(), now.Nanosecond(), now.Location()), nil
+}
+
+// BuildResponse builds response to IFTTT
+func BuildResponse(isOverLimit bool, greenPercentage float32) ([]byte, error) {
+	return json.Marshal(Response{
+		data: []ResponseDetail{
+			ResponseDetail{
+				isOverLimit:     isOverLimit,
+				greenPercentage: greenPercentage,
+			},
+		},
+	})
 }
 
 func Handle(e events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
@@ -87,15 +112,15 @@ func Handle(e events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, er
 
 	if now.Before(from) || now.After(to) {
 		fmt.Println("Exiting early outside of range")
-		return events.APIGatewayProxyResponse{Body: "false", StatusCode: 200}, nil
+		body, _ := BuildResponse(false, 0.0)
+
+		return events.APIGatewayProxyResponse{Body: string(body), StatusCode: 200}, nil
 	}
 
 	response, err := http.Get("http://api.carbonintensity.org.uk/generation")
-
 	if err != nil {
 		fmt.Printf("The HTTP request failed with error %s\n", err)
-		// Exit early here
-		return events.APIGatewayProxyResponse{Body: "false", StatusCode: 200}, nil
+		return events.APIGatewayProxyResponse{StatusCode: 500}, nil
 	}
 
 	jsonData, _ := ioutil.ReadAll(response.Body)
@@ -104,15 +129,17 @@ func Handle(e events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, er
 	err = json.Unmarshal(jsonData, &result)
 
 	if err != nil {
-		fmt.Printf("Error %s\n", err)
+		fmt.Printf("Error Unmarshaling json %s\n", err)
 		return events.APIGatewayProxyResponse{StatusCode: 500}, nil
 	}
 
 	aggregation := result.Data.Mix.AggregateGreenEnergy()
 
-	isHigher := strconv.FormatBool(aggregation > 30.0)
+	isHigher := aggregation > 30.0
 
-	return events.APIGatewayProxyResponse{Body: isHigher, StatusCode: 200}, nil
+	body, _ := BuildResponse(isHigher, aggregation)
+
+	return events.APIGatewayProxyResponse{Body: string(body), StatusCode: 200}, nil
 }
 
 func main() {

ab48e091cfb57c186e723079f8725777d99d8767
Thomas Nairn <tjnairn@gmail.com>
2018-06-15 02:19:13 +0100

    Added serverless and auth check

--- auth/auth-check.go
+++ auth/auth-check.go
@@ -6,7 +6,13 @@ import (
 )
 
 func ValidateServiceKey(key string) error {
-	if key != os.Getenv("IFTTT_KEY") {
+	if os.Getenv("IFTTT_SERVICE_KEY") == "" {
+		return errors.New("No environment variable specified when deploying this lambda")
+	}
+	if key == "" {
+		return errors.New("No Service Key Given")
+	}
+	if key != os.Getenv("IFTTT_SERVICE_KEY") {
 		return errors.New("Invalid Service Key")
 	}
 	return nil
--- cmd/status/main.go
+++ cmd/status/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"log"
+	"os"
+
+	auth "github.com/Tinee/hackathon2018/auth"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func Handle(e events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Println(e.Headers["ifttt-service-key"])
+	log.Println(os.Getenv("IFTTT_SERVICE_KEY"))
+	err := auth.ValidateServiceKey(e.Headers["ifttt-service-key"])
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: "\"" + err.Error() + "\"", StatusCode: 401}, nil
+	}
 	return events.APIGatewayProxyResponse{StatusCode: 200}, nil
 }
 
